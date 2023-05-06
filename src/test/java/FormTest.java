f94b82fad1c743589dcaef54b4a66cd38e68b871
Alexey Kovalev <amkovalyov@yandex.ru>
2023-05-06 19:43:15 +0300

    change build

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FormTest {
     private WebDriver driver;
