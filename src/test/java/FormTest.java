37e4df203df1e4e7abc20a2a3eb044b623d7fd3d
Alexey Kovalev <amkovalyov@yandex.ru>
2023-05-06 19:12:26 +0300

    Add test

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,26 +1,28 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FormTest {
     private WebDriver driver;
+
     @BeforeAll
     static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "./webdriver/chromedriver.exe");
     }
+
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
     }
+
     @AfterEach
     public void tearDown() {
         driver.quit();
         driver = null;
     }
+
+    @Test
     public void shouldTestForm() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ковалев Алексей");
